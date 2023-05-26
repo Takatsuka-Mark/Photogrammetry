6e6232066a804f6efde66f161e7fcd8d798effe0
Takatsuka-Mark <takatsuka.mark@gmail.com>
2023-05-26 00:41:52 -0400

    Video Streaming! Some horizontal artifacts...

--- scripts/take_img.py
+++ scripts/take_img.py
@@ -1,5 +1,17 @@
+from time import sleep
+from picamera import PiCamera
+
+
 def main():
-    pass
+    print("Booting camera")
+    cam = PiCamera()
+    cam.resolution = (1920, 1080)
+    # cam.start_preview()
+    sleep(2)
+    for i in range(100):
+        cam.capture(f'./data/test{i}.jpg')
+        sleep(0.5)
+    # print("Image Captured")
 
 
 if __name__ == '__main__':
