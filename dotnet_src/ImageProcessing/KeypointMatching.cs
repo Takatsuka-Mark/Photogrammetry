aa61fbd39ec7c3324fa476995d2e22482a847a66
Takatsuka-Mark <takatsuka.mark@gmail.com>
2024-06-19 20:51:41 -0700

    Start keypoint matching

---
+++ dotnet_src/ImageProcessing/KeypointMatching.cs
@@ -0,0 +1,16 @@
+using ImageProcessing.Abstractions;
+
+namespace ImageProcessing;
+
+public class KeypointMatching
+{
+    public KeypointMatching()
+    {
+        
+    }
+
+    public static int MatchKeypoints(List<Keypoint> keypoints1, List<Keypoint> keypoints2)
+    {
+        
+    }
+}
