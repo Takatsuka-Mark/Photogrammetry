7d9269c01931e67b533f099ea3811d41c22dd65a
Takatsuka-Mark <takatsuka.mark@gmail.com>
2024-11-11 22:16:15 -0800

    RANSAC. Needs point normalization.

--- dotnet_src/ImageProcessing/CameraPoseEstimation.cs
+++ dotnet_src/ImageProcessing/CameraPoseEstimation.cs
@@ -22,33 +22,48 @@ public class CameraPoseEstimation{
     }
 
 
-    public void OutlierRejection(IList<KeypointPair> keypointPairs, int numSamples, int numPairsPerSample){
+    public List<KeypointPair> OutlierRejection(IList<KeypointPair> keypointPairs, int numSamples, int numPairsPerSample, float threshold){
         if (numPairsPerSample < 8)
             throw new InvalidOperationException("At least 8 keypoint pairs must be included per sample");
         
         if (keypointPairs.Count < numPairsPerSample)
             throw new InvalidOperationException("Must provide at least as many keypoint pairs as pairs required per sample");
         
+        // See https://cmsc426.github.io/sfm/#featmatch for ransac impl
         var random = new Random();
-
-        var n = 0;
-        var includedSample = new List<KeypointPair>();
+        var bestSample = new List<KeypointPair>();
         for (var sampleIdx = 0; sampleIdx < numSamples; sampleIdx += 1){
 
             var samples = keypointPairs.OrderBy(_ => random.NextSingle()).Take(numPairsPerSample).ToList();
 
             var f = EstimateFundamentalMatrix(samples);
+            var workingPairs = new List<KeypointPair>();
 
-            throw new NotImplementedException();
-            // https://cmsc426.github.io/sfm/#featmatch for ransac
+            foreach (var keypointPair in keypointPairs){
+                var kp1Mat = Matrix<float>.Build.DenseOfRowMajor(3, 1, new List<float>() {keypointPair.Keypoint2.Coordinate.X, keypointPair.Keypoint2.Coordinate.Y, 1});    // TODO if needed for perf (and not clarity), just build transposed.
+                var kp2Mat = Matrix<float>.Build.DenseOfRowMajor(3, 1, new List<float>() {keypointPair.Keypoint1.Coordinate.X, keypointPair.Keypoint1.Coordinate.Y, 1});
+                if (kp2Mat.Transpose().Multiply(f.Multiply(kp1Mat))[0, 0] <= threshold){
+                    workingPairs.Add(keypointPair);
+                }
+            }
+
+            if (workingPairs.Count > bestSample.Count){
+                // TODO should this also have an out var for the best fundamental matrix?
+                bestSample = workingPairs;
+            }
         }
+
+        return bestSample;
     }
 
-    public Matrix<float> EstimateFundamentalMatrix(IList<KeypointPair> keypointPairs){
+    internal Matrix<float> EstimateFundamentalMatrix(IList<KeypointPair> keypointPairs){
         if (keypointPairs.Count < 8)
             throw new InvalidOperationException("At least 8 keypoint pairs must be provided");
         
         var mat = new DenseMatrix(keypointPairs.Count, 9);
+
+        throw new NotImplementedException();
+        // TODO should actually normalize these values, from -1 - 1. See 4.1 of https://web.stanford.edu/class/cs231a/course_notes/03-epipolar-geometry.pdf
         
         for (var i = 0; i < keypointPairs.Count; i += 1){
             var kpPair = keypointPairs[i];
